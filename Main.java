48b0f020c7eb316399b1d23f668f2c875ea8ccd5
Ali Hamzayev <alihmzyv@gmail.com>
2024-01-28 12:23:18 +0400

    some 2nd not bug introducing commit to use for bisect

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		//this is a comment, so it did not introduce a bug
 		System.out.println("Hello Git!");
+		//this is the 2nd comment, so it did not introduce a bug
 	}
 }
